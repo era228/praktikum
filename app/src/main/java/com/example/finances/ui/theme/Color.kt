672668f7018680cd043fbe37e0c158cbb5fda55e
PigPunkDev <ivan.khodosevich@bk.ru>
2024-05-24 00:24:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/finances/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.finances.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple1 = Color(0xFF03A9F4)
+val Purple2 = Color(0xFF03A9F4)
+val Purple3 = Color(0xFF03A9F4)
+val IncomeColor = Color(0xFF2BDA2B)
+val ExpenseColor = Color(0xFFA51717)
